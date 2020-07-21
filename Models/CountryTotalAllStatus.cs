b88baaf0c32392c9ae5509c2caf145862b4abf5d
Arka Chattopadhyay <admcloudknights@gmail.com>
2020-07-21 14:43:34 +0530

    Dashboard changes

---
+++ Models/CountryTotalAllStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Covignite.Models
+{
+	public class CountryTotalAllStatus
+	{
+		public long Confirmed { get; set; }
+		public long Deaths { get; set; }
+		public long Recovered { get; set; }
+		public long Active { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
