a64663bb4b5c870d15377bcc1c28b163d7a01493
Arka Chattopadhyay <admcloudknights@gmail.com>
2020-07-21 13:32:50 +0530

    Dashboard related changes

---
+++ Models/CountryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Covignite.Models
+{
+	public class CountryModel
+	{
+		public string Country { get; set; }
+		public string Slug { get; set; }
+		public string ISO2 { get; set; }
+
+	}
+}
---
+++ Models/SelectModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Covignite.Models
+{
+	public class SelectModel
+	{
+		public string ValueField { get; set; }
+		public string TextField { get; set; }
+	}
+}
